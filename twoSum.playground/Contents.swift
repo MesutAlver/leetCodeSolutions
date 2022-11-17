df467b2447385bfa6274fb31a647ea164611b3f7
MesutAlverü <mesutalver@icloud.com>
2022-11-17 21:53:57 +0300

    fizzBuzz Solutions

---
+++ twoSum.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+    
+   
+    
+class Solution {
+     
+    func fizzBuzz(_ n: Int) -> [String] {
+       
+        var fizzBuzzArray = [String]()
+        if n < 0 {
+            return fizzBuzzArray
+        }
+        
+        for i in 1...n {
+            if i % 3 == 0 && i % 5 == 0 {
+                fizzBuzzArray.append("FizzBuzz")
+                
+            } else if i % 3 == 0 {
+                fizzBuzzArray.append("Fizz")
+            } else if i % 5 == 0 {
+                fizzBuzzArray.append("Buzz")
+            } else {
+                
+                fizzBuzzArray.append("\(i)")
+            }
+        }
+        
+        return fizzBuzzArray
+        
+    }
+}
